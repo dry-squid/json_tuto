4d3ff0346f8d0b6d3bf958db06a1446270f14d9c
냉덩오징어 <sadudals33@gmail.com>
2023-11-10 12:09:26 +0900

    json route 제작

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
-
+from collections import Counter
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '82세 곽춘덕 할아버지의 혼신을담은 마지막사이드와인더 발사!'
+    user_string = '사이드와인더a 사이드와인더b 사이드와인더c 사이드와인더d 사이드와인더f'
+    counter = dict(Counter(user_string))
+    result = json.dumps(counter)
+
+    return result
 
